977d66c4db4deb924ea905fc766120182ad971a9
PeeHaa <info@pieterhordijk.com>
2013-02-11 01:56:54 +0100

    forgot to attribute good guy dave

--- src/WebSocketServer/Socket/Frame/Encoder.php
+++ src/WebSocketServer/Socket/Frame/Encoder.php
@@ -8,6 +8,7 @@
  * @package    Socket
  * @subpackage Frame
  * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @author     DaveRandom <https://github.com/DaveRandom>
  * @copyright  Copyright (c) 2013 Pieter Hordijk
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    1.0.0
